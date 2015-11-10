14fbe238390d4c8d74b8033f5a046a03dec602a4
Wellington <wellington.schu@live.com>
2015-11-10 10:12:26 -0200

    Primeira Submissão

--- src/Recursividade/somaProdutoVetor.java
+++ src/Recursividade/somaProdutoVetor.java
@@ -1,5 +1,23 @@
 package Recursividade;
 
 public class somaProdutoVetor {
+	
+	public static double soma(double[] vetor){
+		if(vetor.length == 1)
+			return vetor[0];
+		else{
+			
+			return vetor[0] + vetor [vetor.length-1];
+		}
+	}
+	
+	public static double multiplicacao(double[] vetor){
+		if(vetor.length == 1)
+			return vetor[0];
+		else{
+			
+			return vetor[0] * vetor [vetor.length-1];
+		}
+	}
 
 }
